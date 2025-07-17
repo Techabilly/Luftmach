c915dc1036c6ce639a2e56e3879292d53a93da9d
Techabilly <165205263+Techabilly@users.noreply.github.com>
2025-07-17 10:52:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/Techabilly/Luftmach/', // Must match your repo name
+    base: '/Luftmach/', // Must match your repo name
 })
