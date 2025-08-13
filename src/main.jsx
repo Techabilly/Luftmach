30e9bdf91fc87b45f19c66532d6a14462392cc5e
Techabilly <165205263+Techabilly@users.noreply.github.com>
2025-08-12 22:58:19 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { UiProvider } from './ui/UiContext.jsx';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <UiProvider>
+      <App />
+    </UiProvider>
+  </React.StrictMode>
+);
